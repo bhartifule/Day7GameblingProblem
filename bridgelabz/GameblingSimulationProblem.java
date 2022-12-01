fd0c5d192a214f0bd429b3097dd990216d75191b
Bharti <bharatifule10gmail.com>
2022-12-01 23:12:00 +0530

    winn loss added

--- bridgelabz/GameblingSimulationProblem.java
+++ bridgelabz/GameblingSimulationProblem.java
@@ -1,10 +1,23 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class GameblingSimulationProblem {
-	 public static void main(String[] args) {
-	        System.out.println("Welcome in Gambling Simulation Problem");
-	        int stake = 100;
-	        int bet = 1;
-	        System.out.println("start with a stake of " + stake +" every day and bet " + bet +" every game.");
-	    }
+	public static void main(String[] args) {
+        System.out.println("Welcome in Gambling Simulation Problem");
+        int stake = 100;
+        int bet = 1;
+        Random random = new Random();
+       int num = random.nextInt(2) + 0;
+
+        if (num == 1){
+            stake += bet;
+            System.out.println("gambler is win by "+ bet+" & total Stake is " + stake);
+
+        }
+        else if (num == 0){
+            stake -=bet;
+            System.out.println("gambler is loose by "+bet+" & total Stake is "+ stake );
+        }
+    }
 }
